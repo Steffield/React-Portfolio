73a1b32ca6dde52b5ff5d9668cb4e4f0b5c5300f
Steffield <steffilebby@gmail.com>
2020-05-16 20:07:19 -0400

    Updates

---
+++ precache-manifest.327839d5eb9f484aacedffa7e525ff94.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d87f96fe2cf4caa9fca6704b959ee4",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4611e94e1e007cc29c0e",
+    "url": "/React-Portfolio/static/css/main.efbe2fc2.chunk.css"
+  },
+  {
+    "revision": "1d80a2c867ff2b10e02a",
+    "url": "/React-Portfolio/static/js/2.785d0772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.785d0772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4611e94e1e007cc29c0e",
+    "url": "/React-Portfolio/static/js/main.4e4300f2.chunk.js"
+  },
+  {
+    "revision": "ffe32e766840acea84c9",
+    "url": "/React-Portfolio/static/js/runtime-main.3c52bd9a.js"
+  },
+  {
+    "revision": "f80afaacc74ff21426c782dbb41922b8",
+    "url": "/React-Portfolio/static/media/CQ.f80afaac.png"
+  },
+  {
+    "revision": "e5839d65de80f667805de5d6ffba8b46",
+    "url": "/React-Portfolio/static/media/EmpDir.e5839d65.png"
+  },
+  {
+    "revision": "5a4d4a7fcd7349710bcd5ef6ced50c9c",
+    "url": "/React-Portfolio/static/media/KYV.5a4d4a7f.png"
+  },
+  {
+    "revision": "f34f839ca8ede729ad8823a6dcc0f22a",
+    "url": "/React-Portfolio/static/media/OfflineBudget.f34f839c.png"
+  },
+  {
+    "revision": "7ae62e4a1fc523a706ec059e9f20f684",
+    "url": "/React-Portfolio/static/media/Planner.7ae62e4a.png"
+  },
+  {
+    "revision": "9d6049cf2a00f413e9da158b7c738e76",
+    "url": "/React-Portfolio/static/media/WA.9d6049cf.png"
+  },
+  {
+    "revision": "e45961ac9268611bac4a5bb0715c28df",
+    "url": "/React-Portfolio/static/media/WorkoutTracker.e45961ac.png"
+  },
+  {
+    "revision": "66b9268e617f983a6c6878e664080caf",
+    "url": "/React-Portfolio/static/media/burger.66b9268e.png"
+  },
+  {
+    "revision": "c906cc3007920e97d17efac5cd5bbc90",
+    "url": "/React-Portfolio/static/media/contact.c906cc30.png"
+  },
+  {
+    "revision": "0464aef30cb11f9dfadc6316ce5c9818",
+    "url": "/React-Portfolio/static/media/lh.0464aef3.png"
+  },
+  {
+    "revision": "22bef38e0e3d43a55d4d78b502bf3fca",
+    "url": "/React-Portfolio/static/media/me.22bef38e.png"
+  },
+  {
+    "revision": "5bc85efb9a7d7d0b8802e747b71c7020",
+    "url": "/React-Portfolio/static/media/work.5bc85efb.png"
+  }
+]);
