a3de0705667569bdc156af45044f4101714bb34b
Steffield <steffilebby@gmail.com>
2022-09-14 04:16:34 -0400

    removed trailhead link on home page

--- src/components/SectionImage/MainSectionImage.js
+++ src/components/SectionImage/MainSectionImage.js
@@ -70,9 +70,9 @@ function MainSectionImage() {
           <a href="https://www.linkedin.com/in/stephanie-lebby">
             <i className="fa fa-linkedin iconsTop"></i>
           </a>
-          <a href="https://www.linkedin.com/in/stephanie-lebby">
+          {/* <a href="https://www.linkedin.com/in/stephanie-lebby">
             <i className="fa fa-salesforce iconsTop"></i>
-          </a>
+          </a> */}
         </div>
       </div>
     </>
